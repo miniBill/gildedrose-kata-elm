091164a3561e3a777e2f5270d423b8500f62beea
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-08-17 15:01:56 +0200

    Make code more idiomatic

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -8,43 +8,42 @@ type alias Item =
     }
 
 
-updateItemQuality : { name : String, sell_by : Int, quality : Int } -> { name : String, sell_by : Int, quality : Int }
-updateItemQuality =
-    \item ->
-        if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert" then
-            if item.quality < 50 then
-                if item.name == "Backstage passes to a TAFKAL80ETC concert" then
-                    if item.sell_by < 0 then
-                        { item | sell_by = item.sell_by - 1, quality = 0 }
+updateItemQuality : Item -> Item
+updateItemQuality item =
+    if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert" then
+        if item.quality < 50 then
+            if item.name == "Backstage passes to a TAFKAL80ETC concert" then
+                if item.sell_by < 0 then
+                    { item | sell_by = item.sell_by - 1, quality = 0 }
 
-                    else if item.sell_by < 6 then
-                        { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
+                else if item.sell_by < 6 then
+                    { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
 
-                    else if item.sell_by < 11 then
-                        { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
-
-                    else
-                        { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+                else if item.sell_by < 11 then
+                    { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
 
                 else
                     { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
 
             else
-                item
+                { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
 
-        else if item.name /= "Aged Brie" && item.name /= "Sulfuras, Hand of Ragnaros" then
-            if item.sell_by < 0 && item.quality > 0 then
-                if item.quality >= 2 then
-                    { item | sell_by = item.sell_by - 1, quality = item.quality - 2 }
+        else
+            item
 
-                else
-                    { item | sell_by = item.sell_by - 1, quality = 0 }
-
-            else if item.quality >= 1 then
-                { item | sell_by = item.sell_by - 1, quality = item.quality - 1 }
+    else if item.name /= "Aged Brie" && item.name /= "Sulfuras, Hand of Ragnaros" then
+        if item.sell_by < 0 && item.quality > 0 then
+            if item.quality >= 2 then
+                { item | sell_by = item.sell_by - 1, quality = item.quality - 2 }
 
             else
                 { item | sell_by = item.sell_by - 1, quality = 0 }
 
+        else if item.quality >= 1 then
+            { item | sell_by = item.sell_by - 1, quality = item.quality - 1 }
+
         else
-            item
+            { item | sell_by = item.sell_by - 1, quality = 0 }
+
+    else
+        item
