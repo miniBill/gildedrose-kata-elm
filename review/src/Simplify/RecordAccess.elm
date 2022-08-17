22fa58bca4f0fb569ea30f5e00b019229c02bf98
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-08-17 14:58:19 +0200

    Also simplify on record updates

--- review/src/Simplify/RecordAccess.elm
+++ review/src/Simplify/RecordAccess.elm
@@ -6,7 +6,7 @@ module Simplify.RecordAccess exposing (rule)
 
 -}
 
-import Elm.Syntax.Expression as Expression exposing (Expression)
+import Elm.Syntax.Expression as Expression exposing (Expression, RecordSetter)
 import Elm.Syntax.Node as Node exposing (Node)
 import Elm.Writer
 import List.Extra
@@ -65,35 +65,43 @@ expressionVisitor node =
             in
             case Node.value record of
                 Expression.RecordExpr setters ->
-                    let
-                        setterValues =
-                            List.map
-                                (\setter ->
-                                    let
-                                        ( nfield, nvalue ) =
-                                            Node.value setter
-                                    in
-                                    ( Node.value nfield, nvalue )
-                                )
-                                setters
-                    in
-                    case
-                        List.Extra.find (\( setterField, _ ) -> setterField == fieldName) setterValues
-                    of
-                        Just ( _, setterValue ) ->
-                            [ Rule.errorWithFix
-                                { message = "Field access can be simplified"
-                                , details = [ "Accessing the field of a known record can be simplified to just that field's value" ]
-                                }
-                                (Node.range node)
-                                [ Fix.replaceRangeBy (Node.range node) (Elm.Writer.write <| Elm.Writer.writeExpression setterValue) ]
-                            ]
+                    trySimplify setters fieldName node
 
-                        Nothing ->
-                            []
+                Expression.RecordUpdateExpression _ setters ->
+                    trySimplify setters fieldName node
 
                 _ ->
                     []
 
         _ ->
+            []
+
+
+trySimplify : List (Node RecordSetter) -> String -> Node Expression -> List (Rule.Error {})
+trySimplify setters fieldName node =
+    let
+        setterValues =
+            List.map
+                (\setter ->
+                    let
+                        ( nfield, nvalue ) =
+                            Node.value setter
+                    in
+                    ( Node.value nfield, nvalue )
+                )
+                setters
+    in
+    case
+        List.Extra.find (\( setterField, _ ) -> setterField == fieldName) setterValues
+    of
+        Just ( _, setterValue ) ->
+            [ Rule.errorWithFix
+                { message = "Field access can be simplified"
+                , details = [ "Accessing the field of a known record can be simplified to just that field's value" ]
+                }
+                (Node.range node)
+                [ Fix.replaceRangeBy (Node.range node) (Elm.Writer.write <| Elm.Writer.writeExpression setterValue) ]
+            ]
+
+        Nothing ->
             []
--- review/tests/Simplify/RecordAccessTest.elm
+++ review/tests/Simplify/RecordAccessTest.elm
@@ -26,4 +26,36 @@ a = { b = 3 }.b
 a = 3
 """
                         ]
+        , test "shouldn't simplify record accesses for explicit records if it can't find the field" <|
+            \() ->
+                """module A exposing (..)
+a = { b = 3 }.c
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should simplify record accesses for record updates" <|
+            \() ->
+                """module A exposing (..)
+a = { d | b = 3 }.b
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Field access can be simplified"
+                            , details =
+                                [ "Accessing the field of a known record can be simplified to just that field's value"
+                                ]
+                            , under = "{ d | b = 3 }.b"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = 3
+"""
+                        ]
+        , test "shouldn't simplify record accesses for record updates if it can't find the field" <|
+            \() ->
+                """module A exposing (..)
+a = { d | b = 3 }.c
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
         ]
