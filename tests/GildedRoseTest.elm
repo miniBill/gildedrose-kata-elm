9855436d4f4180ceb80f45417c9fbe6633eced46
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-08-20 14:43:06 +0200

    Switch tests to the "official" ones

--- tests/GildedRoseTest.elm
+++ tests/GildedRoseTest.elm
@@ -1,183 +1,450 @@
-module GildedRoseTest exposing (initial)
+module GildedRoseTest exposing (suite)
 
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer)
+import Expect
 import GildedRose exposing (Item, updateItemQuality)
-import Test exposing (Test, describe, fuzz)
+import Test exposing (Test, describe, test)
 
 
-initial : Test
-initial =
-    describe "Initial behaviour"
-        [ describe "Generic items"
-            [ testGeneric
-                "sell_in decreases every day"
-                (\item updated -> Expect.lessThan item.sell_by updated.sell_by)
-            , testGeneric
-                "quality decreses every day if positive"
-                (\item updated ->
-                    if item.quality == 0 then
-                        Expect.equal 0 updated.quality
+suite : Test
+suite =
+    textBasedTestSource
+        |> String.split "\n\n"
+        |> List.indexedMap parseDay
+        |> List.foldl
+            (\( dayNumber, day ) ( maybePreviousDay, tests ) ->
+                case maybePreviousDay of
+                    Nothing ->
+                        ( Just day, tests )
 
-                    else
-                        Expect.lessThan item.quality updated.quality
-                )
-            , testGeneric
-                "quality decreses twice as fast if the sell_by date has passed"
-                (\item updated ->
-                    Expect.equal
-                        (case item.quality of
-                            0 ->
-                                0
-
-                            1 ->
-                                0
-
-                            _ ->
-                                if item.sell_by < 0 then
-                                    item.quality - 2
-
-                                else
-                                    item.quality - 1
+                    Just previousDay ->
+                        let
+                            test =
+                                describe
+                                    ("On day " ++ String.fromInt dayNumber)
+                                    (List.map2 toTest previousDay day)
+                        in
+                        ( Just day
+                        , test :: tests
                         )
-                        updated.quality
-                )
-            ]
-        , fuzz itemFuzzer
-            "The quality of an item is never negative"
-            (Expect.all
-                [ \item -> Expect.atLeast 0 item.quality
-                , \item ->
-                    Expect.atLeast 0 (updateItemQuality item).quality
-                ]
             )
-        , testSpecific "Aged Brie"
-            "The quality of Aged Brie increases the older it gets (up to 50)"
-            (\item updated ->
-                if item.quality == 50 then
-                    Expect.equal 50 updated.quality
-
-                else
-                    Expect.greaterThan item.quality updated.quality
-            )
-        , fuzz itemFuzzer
-            "The quality of an item is never more than 50"
-            (\item -> Expect.atMost 50 (updateItemQuality item).quality)
-        , testSpecific "Sulfuras, Hand of Ragnaros"
-            "Sulfuras' quality never decreases"
-            (\item updated -> Expect.equal item.quality updated.quality)
-        , testSpecific "Backstage passes to a TAFKAL80ETC concert"
-            "Backstage passes get more quality as the day approaches, but are then worthless"
-            (\item updated ->
-                if item.quality >= 48 then
-                    -- Backstage passes will not get more quality than 48
-                    Expect.pass
-
-                else if item.sell_by < 0 then
-                    Expect.equal 0 updated.quality
-
-                else if item.sell_by <= 5 then
-                    Expect.equal (clamp 0 50 <| item.quality + 3) updated.quality
-
-                else if item.sell_by <= 10 then
-                    Expect.equal (clamp 0 50 <| item.quality + 2) updated.quality
-
-                else
-                    Expect.equal (clamp 0 50 <| item.quality + 1) updated.quality
-            )
-        , testSpecific "Conjured staff"
-            "Conjured items degrade twice as fast"
-            (\item updated ->
-                Expect.equal
-                    (case item.quality of
-                        0 ->
-                            0
-
-                        1 ->
-                            0
-
-                        2 ->
-                            0
-
-                        3 ->
-                            if item.sell_by < 0 then
-                                0
-
-                            else
-                                1
-
-                        _ ->
-                            if item.sell_by < 0 then
-                                item.quality - 4
-
-                            else
-                                item.quality - 2
-                    )
-                    updated.quality
-            )
-        ]
+            ( Nothing, [] )
+        |> Tuple.second
+        |> List.reverse
+        |> describe "Check updateItemQuality"
 
 
-testSpecific : String -> String -> (Item -> Item -> Expectation) -> Test
-testSpecific name description toExpectation =
-    fuzz (specificItem name)
-        description
-        (\item ->
-            toExpectation item (updateItemQuality item)
+toTest : Item -> Item -> Test
+toTest previous current =
+    test
+        (itemToString previous ++ " should become " ++ itemToString current)
+        (\() ->
+            updateItemQuality previous
+                |> Expect.equal current
         )
 
 
-testGeneric : String -> (Item -> Item -> Expectation) -> Test
-testGeneric description toExpectation =
-    fuzz genericItemFuzzer
-        description
-        (\item ->
-            toExpectation item (updateItemQuality item)
-        )
+itemToString : Item -> String
+itemToString item =
+    item.name ++ ", " ++ String.fromInt item.sellIn ++ ", " ++ String.fromInt item.quality
 
 
-genericItemFuzzer : Fuzzer Item
-genericItemFuzzer =
-    specificItem "Foo"
+parseDay : Int -> String -> ( Int, List Item )
+parseDay index day =
+    ( index
+    , day
+        |> String.split "\n"
+        |> List.drop 2
+        |> List.filterMap parseItem
+    )
 
 
-specificItem : String -> Fuzzer Item
-specificItem name =
-    Fuzz.map3 Item
-        (Fuzz.constant name)
-        sellByFuzzer
-        qualityFuzzer
+parseItem : String -> Maybe Item
+parseItem line =
+    case String.split ", " line of
+        [ name, sell_by_string, quality_string ] ->
+            case ( String.toInt sell_by_string, String.toInt quality_string ) of
+                ( Just sell_by, Just quality ) ->
+                    Just { name = name, sellIn = sell_by, quality = quality }
+
+                _ ->
+                    Nothing
+
+        _ ->
+            Nothing
 
 
-itemFuzzer : Fuzzer Item
-itemFuzzer =
-    Fuzz.map3
-        Item
-        nameFuzzer
-        sellByFuzzer
-        qualityFuzzer
+textBasedTestSource : String
+textBasedTestSource =
+    """
+-------- day 0 --------
+name, sellIn, quality
++5 Dexterity Vest, 10, 20
+Aged Brie, 2, 0
+Elixir of the Mongoose, 5, 7
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 15, 20
+Backstage passes to a TAFKAL80ETC concert, 10, 49
+Backstage passes to a TAFKAL80ETC concert, 5, 49
+Conjured Mana Cake, 3, 6
 
+-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 19
+Aged Brie, 1, 1
+Elixir of the Mongoose, 4, 6
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 14, 21
+Backstage passes to a TAFKAL80ETC concert, 9, 50
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+Conjured Mana Cake, 2, 5
 
-nameFuzzer : Fuzzer String
-nameFuzzer =
-    -- There are three names which are handled differently, then we add a generic string fuzzer for non-specific names
-    Fuzz.oneOf
-        [ Fuzz.constant "Aged Brie"
-        , Fuzz.constant "Backstage passes to a TAFKAL80ETC concert"
-        , Fuzz.constant "Sulfuras, Hand of Ragnaros"
-        , Fuzz.string
-        ]
+-------- day 2 --------
+name, sellIn, quality
++5 Dexterity Vest, 8, 18
+Aged Brie, 0, 2
+Elixir of the Mongoose, 3, 5
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 13, 22
+Backstage passes to a TAFKAL80ETC concert, 8, 50
+Backstage passes to a TAFKAL80ETC concert, 3, 50
+Conjured Mana Cake, 1, 4
 
+-------- day 3 --------
+name, sellIn, quality
++5 Dexterity Vest, 7, 17
+Aged Brie, -1, 4
+Elixir of the Mongoose, 2, 4
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 12, 23
+Backstage passes to a TAFKAL80ETC concert, 7, 50
+Backstage passes to a TAFKAL80ETC concert, 2, 50
+Conjured Mana Cake, 0, 3
 
-sellByFuzzer : Fuzzer Int
-sellByFuzzer =
-    -- The critical points are 0, 6 and 11
-    Fuzz.oneOf (List.map Fuzz.constant <| List.range -2 12)
+-------- day 4 --------
+name, sellIn, quality
++5 Dexterity Vest, 6, 16
+Aged Brie, -2, 6
+Elixir of the Mongoose, 1, 3
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 11, 24
+Backstage passes to a TAFKAL80ETC concert, 6, 50
+Backstage passes to a TAFKAL80ETC concert, 1, 50
+Conjured Mana Cake, -1, 1
 
+-------- day 5 --------
+name, sellIn, quality
++5 Dexterity Vest, 5, 15
+Aged Brie, -3, 8
+Elixir of the Mongoose, 0, 2
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 10, 25
+Backstage passes to a TAFKAL80ETC concert, 5, 50
+Backstage passes to a TAFKAL80ETC concert, 0, 50
+Conjured Mana Cake, -2, 0
 
-qualityFuzzer : Fuzzer Int
-qualityFuzzer =
-    -- Critical points are 0, 1, 2, and 50
-    -- We don't include -1 because the problem input says the quality is always nonnegative
-    -- We don't include 51 because the problem input says the quality is never over 50. Except for "Sulfuras"
-    Fuzz.oneOf (List.map Fuzz.constant <| List.range 0 50)
+-------- day 6 --------
+name, sellIn, quality
++5 Dexterity Vest, 4, 14
+Aged Brie, -4, 10
+Elixir of the Mongoose, -1, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 9, 27
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+Backstage passes to a TAFKAL80ETC concert, -1, 0
+Conjured Mana Cake, -3, 0
+
+-------- day 7 --------
+name, sellIn, quality
++5 Dexterity Vest, 3, 13
+Aged Brie, -5, 12
+Elixir of the Mongoose, -2, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 8, 29
+Backstage passes to a TAFKAL80ETC concert, 3, 50
+Backstage passes to a TAFKAL80ETC concert, -2, 0
+Conjured Mana Cake, -4, 0
+
+-------- day 8 --------
+name, sellIn, quality
++5 Dexterity Vest, 2, 12
+Aged Brie, -6, 14
+Elixir of the Mongoose, -3, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 7, 31
+Backstage passes to a TAFKAL80ETC concert, 2, 50
+Backstage passes to a TAFKAL80ETC concert, -3, 0
+Conjured Mana Cake, -5, 0
+
+-------- day 9 --------
+name, sellIn, quality
++5 Dexterity Vest, 1, 11
+Aged Brie, -7, 16
+Elixir of the Mongoose, -4, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 6, 33
+Backstage passes to a TAFKAL80ETC concert, 1, 50
+Backstage passes to a TAFKAL80ETC concert, -4, 0
+Conjured Mana Cake, -6, 0
+
+-------- day 10 --------
+name, sellIn, quality
++5 Dexterity Vest, 0, 10
+Aged Brie, -8, 18
+Elixir of the Mongoose, -5, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 5, 35
+Backstage passes to a TAFKAL80ETC concert, 0, 50
+Backstage passes to a TAFKAL80ETC concert, -5, 0
+Conjured Mana Cake, -7, 0
+
+-------- day 11 --------
+name, sellIn, quality
++5 Dexterity Vest, -1, 8
+Aged Brie, -9, 20
+Elixir of the Mongoose, -6, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 4, 38
+Backstage passes to a TAFKAL80ETC concert, -1, 0
+Backstage passes to a TAFKAL80ETC concert, -6, 0
+Conjured Mana Cake, -8, 0
+
+-------- day 12 --------
+name, sellIn, quality
++5 Dexterity Vest, -2, 6
+Aged Brie, -10, 22
+Elixir of the Mongoose, -7, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 3, 41
+Backstage passes to a TAFKAL80ETC concert, -2, 0
+Backstage passes to a TAFKAL80ETC concert, -7, 0
+Conjured Mana Cake, -9, 0
+
+-------- day 13 --------
+name, sellIn, quality
++5 Dexterity Vest, -3, 4
+Aged Brie, -11, 24
+Elixir of the Mongoose, -8, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 2, 44
+Backstage passes to a TAFKAL80ETC concert, -3, 0
+Backstage passes to a TAFKAL80ETC concert, -8, 0
+Conjured Mana Cake, -10, 0
+
+-------- day 14 --------
+name, sellIn, quality
++5 Dexterity Vest, -4, 2
+Aged Brie, -12, 26
+Elixir of the Mongoose, -9, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 1, 47
+Backstage passes to a TAFKAL80ETC concert, -4, 0
+Backstage passes to a TAFKAL80ETC concert, -9, 0
+Conjured Mana Cake, -11, 0
+
+-------- day 15 --------
+name, sellIn, quality
++5 Dexterity Vest, -5, 0
+Aged Brie, -13, 28
+Elixir of the Mongoose, -10, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 0, 50
+Backstage passes to a TAFKAL80ETC concert, -5, 0
+Backstage passes to a TAFKAL80ETC concert, -10, 0
+Conjured Mana Cake, -12, 0
+
+-------- day 16 --------
+name, sellIn, quality
++5 Dexterity Vest, -6, 0
+Aged Brie, -14, 30
+Elixir of the Mongoose, -11, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -1, 0
+Backstage passes to a TAFKAL80ETC concert, -6, 0
+Backstage passes to a TAFKAL80ETC concert, -11, 0
+Conjured Mana Cake, -13, 0
+
+-------- day 17 --------
+name, sellIn, quality
++5 Dexterity Vest, -7, 0
+Aged Brie, -15, 32
+Elixir of the Mongoose, -12, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -2, 0
+Backstage passes to a TAFKAL80ETC concert, -7, 0
+Backstage passes to a TAFKAL80ETC concert, -12, 0
+Conjured Mana Cake, -14, 0
+
+-------- day 18 --------
+name, sellIn, quality
++5 Dexterity Vest, -8, 0
+Aged Brie, -16, 34
+Elixir of the Mongoose, -13, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -3, 0
+Backstage passes to a TAFKAL80ETC concert, -8, 0
+Backstage passes to a TAFKAL80ETC concert, -13, 0
+Conjured Mana Cake, -15, 0
+
+-------- day 19 --------
+name, sellIn, quality
++5 Dexterity Vest, -9, 0
+Aged Brie, -17, 36
+Elixir of the Mongoose, -14, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -4, 0
+Backstage passes to a TAFKAL80ETC concert, -9, 0
+Backstage passes to a TAFKAL80ETC concert, -14, 0
+Conjured Mana Cake, -16, 0
+
+-------- day 20 --------
+name, sellIn, quality
++5 Dexterity Vest, -10, 0
+Aged Brie, -18, 38
+Elixir of the Mongoose, -15, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -5, 0
+Backstage passes to a TAFKAL80ETC concert, -10, 0
+Backstage passes to a TAFKAL80ETC concert, -15, 0
+Conjured Mana Cake, -17, 0
+
+-------- day 21 --------
+name, sellIn, quality
++5 Dexterity Vest, -11, 0
+Aged Brie, -19, 40
+Elixir of the Mongoose, -16, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -6, 0
+Backstage passes to a TAFKAL80ETC concert, -11, 0
+Backstage passes to a TAFKAL80ETC concert, -16, 0
+Conjured Mana Cake, -18, 0
+
+-------- day 22 --------
+name, sellIn, quality
++5 Dexterity Vest, -12, 0
+Aged Brie, -20, 42
+Elixir of the Mongoose, -17, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -7, 0
+Backstage passes to a TAFKAL80ETC concert, -12, 0
+Backstage passes to a TAFKAL80ETC concert, -17, 0
+Conjured Mana Cake, -19, 0
+
+-------- day 23 --------
+name, sellIn, quality
++5 Dexterity Vest, -13, 0
+Aged Brie, -21, 44
+Elixir of the Mongoose, -18, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -8, 0
+Backstage passes to a TAFKAL80ETC concert, -13, 0
+Backstage passes to a TAFKAL80ETC concert, -18, 0
+Conjured Mana Cake, -20, 0
+
+-------- day 24 --------
+name, sellIn, quality
++5 Dexterity Vest, -14, 0
+Aged Brie, -22, 46
+Elixir of the Mongoose, -19, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -9, 0
+Backstage passes to a TAFKAL80ETC concert, -14, 0
+Backstage passes to a TAFKAL80ETC concert, -19, 0
+Conjured Mana Cake, -21, 0
+
+-------- day 25 --------
+name, sellIn, quality
++5 Dexterity Vest, -15, 0
+Aged Brie, -23, 48
+Elixir of the Mongoose, -20, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -10, 0
+Backstage passes to a TAFKAL80ETC concert, -15, 0
+Backstage passes to a TAFKAL80ETC concert, -20, 0
+Conjured Mana Cake, -22, 0
+
+-------- day 26 --------
+name, sellIn, quality
++5 Dexterity Vest, -16, 0
+Aged Brie, -24, 50
+Elixir of the Mongoose, -21, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -11, 0
+Backstage passes to a TAFKAL80ETC concert, -16, 0
+Backstage passes to a TAFKAL80ETC concert, -21, 0
+Conjured Mana Cake, -23, 0
+
+-------- day 27 --------
+name, sellIn, quality
++5 Dexterity Vest, -17, 0
+Aged Brie, -25, 50
+Elixir of the Mongoose, -22, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -12, 0
+Backstage passes to a TAFKAL80ETC concert, -17, 0
+Backstage passes to a TAFKAL80ETC concert, -22, 0
+Conjured Mana Cake, -24, 0
+
+-------- day 28 --------
+name, sellIn, quality
++5 Dexterity Vest, -18, 0
+Aged Brie, -26, 50
+Elixir of the Mongoose, -23, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -13, 0
+Backstage passes to a TAFKAL80ETC concert, -18, 0
+Backstage passes to a TAFKAL80ETC concert, -23, 0
+Conjured Mana Cake, -25, 0
+
+-------- day 29 --------
+name, sellIn, quality
++5 Dexterity Vest, -19, 0
+Aged Brie, -27, 50
+Elixir of the Mongoose, -24, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -14, 0
+Backstage passes to a TAFKAL80ETC concert, -19, 0
+Backstage passes to a TAFKAL80ETC concert, -24, 0
+Conjured Mana Cake, -26, 0
+
+-------- day 30 --------
+name, sellIn, quality
++5 Dexterity Vest, -20, 0
+Aged Brie, -28, 50
+Elixir of the Mongoose, -25, 0
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, -15, 0
+Backstage passes to a TAFKAL80ETC concert, -20, 0
+Backstage passes to a TAFKAL80ETC concert, -25, 0
+Conjured Mana Cake, -27, 0
+"""
