1949e63fcffc1ac8d95ee3b6e1800852be23c710
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-08-20 14:42:57 +0200

    More faithful translation of original Java code

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -3,47 +3,94 @@ module GildedRose exposing (Item, updateItemQuality)
 
 type alias Item =
     { name : String
-    , sell_by : Int
+    , sellIn : Int
     , quality : Int
     }
 
 
 updateItemQuality : Item -> Item
 updateItemQuality item =
-    if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert" then
-        if item.quality < 50 then
-            if item.name == "Backstage passes to a TAFKAL80ETC concert" then
-                if item.sell_by < 0 then
-                    { item | sell_by = item.sell_by - 1, quality = 0 }
+    let
+        withUpdatedQuality =
+            if
+                (item.name /= "Aged Brie")
+                    && (item.name /= "Backstage passes to a TAFKAL80ETC concert")
+            then
+                if item.quality > 0 then
+                    if item.name /= "Sulfuras, Hand of Ragnaros" then
+                        { item | quality = item.quality - 1 }
 
-                else if item.sell_by < 6 then
-                    { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
-
-                else if item.sell_by < 11 then
-                    { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
+                    else
+                        item
 
                 else
-                    { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+                    item
+
+            else if item.quality < 50 then
+                let
+                    item2 =
+                        { item | quality = item.quality + 1 }
+                in
+                if item2.name == "Backstage passes to a TAFKAL80ETC concert" then
+                    let
+                        item3 =
+                            if item2.sellIn < 11 then
+                                if item2.quality < 50 then
+                                    { item2 | quality = item2.quality + 1 }
+
+                                else
+                                    item2
+
+                            else
+                                item2
+                    in
+                    if item3.sellIn < 6 then
+                        if item3.quality < 50 then
+                            { item3 | quality = item3.quality + 1 }
+
+                        else
+                            item3
+
+                    else
+                        item3
+
+                else
+                    item2
 
             else
-                { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+                item
 
-        else
-            { item | sell_by = item.sell_by }
-
-    else if item.name /= "Aged Brie" && item.name /= "Sulfuras, Hand of Ragnaros" then
-        if item.sell_by < 0 && item.quality > 0 then
-            if item.quality >= 2 then
-                { item | sell_by = item.sell_by - 1, quality = item.quality - 2 }
+        withSulfurasCheck =
+            if item.name /= "Sulfuras, Hand of Ragnaros" then
+                { withUpdatedQuality | sellIn = withUpdatedQuality.sellIn - 1 }
 
             else
-                { item | sell_by = item.sell_by - 1, quality = 0 }
+                withUpdatedQuality
 
-        else if item.quality >= 1 then
-            { item | sell_by = item.sell_by - 1, quality = item.quality - 1 }
+        withSellInCheck =
+            if withSulfurasCheck.sellIn < 0 then
+                if withSulfurasCheck.name /= "Aged Brie" then
+                    if withSulfurasCheck.name /= "Backstage passes to a TAFKAL80ETC concert" then
+                        if withSulfurasCheck.quality > 0 then
+                            if withSulfurasCheck.name /= "Sulfuras, Hand of Ragnaros" then
+                                { withSulfurasCheck | quality = withSulfurasCheck.quality - 1 }
 
-        else
-            { item | sell_by = item.sell_by - 1, quality = 0 }
+                            else
+                                withSulfurasCheck
 
-    else
-        item
+                        else
+                            withSulfurasCheck
+
+                    else
+                        { withSulfurasCheck | quality = withSulfurasCheck.quality - withSulfurasCheck.quality }
+
+                else if withSulfurasCheck.quality < 50 then
+                    { withSulfurasCheck | quality = withSulfurasCheck.quality + 1 }
+
+                else
+                    withSulfurasCheck
+
+            else
+                withSulfurasCheck
+    in
+    withSellInCheck
