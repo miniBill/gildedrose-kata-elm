bc0c01b81b6c3faaa6d293aeefb2959347959b3e
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-07-05 15:17:55 +0200

    Add optimize rule

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -29,6 +29,7 @@ import NoUnused.Modules
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
+import Optimize
 import Review.Rule as Rule exposing (Rule)
 import Simplify
 
@@ -55,4 +56,5 @@ config =
     , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
     , Simplify.rule Simplify.defaults
+    , Optimize.rule
     ]
